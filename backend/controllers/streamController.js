480796a403284e90c68d76893c728860924ad829
Aleksandra500 <aleksandracomagic7@gmail.com>
2025-06-07 12:18:17 +0200

    enabling docker host

---
+++ backend/controllers/streamController.js
@@ -0,0 +1,4 @@
+exports.stream = (req, res) => {
+  console.log('Ruta učitana!');
+  res.send('caoskic');
+};
--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,25 @@
 const express = require('express')
 const cors = require('cors')
+const streamRoutes = require('./routes/streamRoutes')
 require('dotenv').config();
+
+
 const app = express()
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5050
 
 app.use(cors())
 app.use(express.json())
 
+app.use((req, res, next) => {
+  console.log(`Zahtev pristigao: ${req.method} ${req.url}`);
+  next();
+});
 
+app.get('/api/stream', (req, res) => {
+  console.log('Ruta /api/stream je pozvana');
+  res.send('caoskic');
+});
+// app.use('/api/stream', streamRoutes)
 
 app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(PORT, () => console.log(`Server je pokrenut na http://localhost:${PORT}`))
+app.listen(PORT, () => console.log(`Server ovajje pokrenut na http://localhost:${PORT}`))
